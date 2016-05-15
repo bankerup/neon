057fa66812fcf37f322d4cb610b59ad4988d664f
bankerup <bankerup@gmail.com>
2016-05-15 11:00:40 +0300

    work in progress

--- resources/script/main.js
+++
@@ -1,5 +0,0 @@
-var toggle = document.getElementById("toggle");
-var menu = document.getElementById("menu");
-toggle.addEventListener("click", function(event){
-    menu.style.display = (menu.style.display == "block" ? "" : "block");
-});
